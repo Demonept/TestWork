134f980a3407f29f82a910b6dd9bb8190fa4a591
Demonept <svk.dmitriy@gmail.com>
2021-04-16 17:03:23 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+function create(a, b, c){
+    let conTitle = document.createElement('div')
+    conTitle.className = 'content_title'
+    let conText = document.createElement('div')
+    conText.className = 'content_text'
+    let conImg = document.createElement('img')
+    conImg.className = 'content_img'
+    conImg.src = 'img/background.jpg'
+    let doc = document.getElementById('content')
+    let con = document.createElement('div')
+    con.className = 'content_item'
+    doc.appendChild(con)
+    con.appendChild(conImg)
+    con.appendChild(conTitle)
+    con.appendChild(conText)
+    conTitle.innerHTML = a
+    conText.innerHTML = b
+    conImg.innerHTML = c
+}
+
+function req() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(json => {
+             json.forEach(post =>{
+                 const title = post.title
+                 const body = post.body
+                 create(title, body)
+
+             })
+        });
+}
+
+req();
+function img(){
+    fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => response.json())
+        .then(json => {
+            json.forEach(photo=>{
+                let q = document.getElementsByClassName('content_img')
+                const img = photo.url
+                q.src = img
+            })
+        })
+}
+
+img();
+
+
